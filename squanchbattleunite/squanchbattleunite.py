865c37f4548e76c770f940b0a90ab595d69258b8
Unknown <jcleto@knights.ucf.edu>
2020-08-04 01:11:23 -0400

    Added in finna command and other stuff.

--- squanchbattleunite/squanchbattleunite.py
+++ squanchbattleunite/squanchbattleunite.py
@@ -27,11 +27,18 @@ RARITY_DICT = {
     4: "<:mhio:738662375401586749>"
 }
 
-RARITY_NAME_DICT = {
-    1 : "Mhoo",
-    2 : "Mini Hoo",
-    3 : "Mega Hoo Incarnate",
-    4 : "My Hoo Is Omega"
+RARITY_STONE_DICT = {
+    1 : "m",
+    2 : "mh",
+    3 : "mhi",
+    4 : "mhio"
+}
+
+RARITY_STONE_EMOJI_DICT ={
+    1 : "<:goutm:739730447612903444>",
+    2 : "<:goutmh:739735718745735169>",
+    3 : "<:goutmhi:739735891903512587>",
+    4 : "<:goutmhio:739736420503257090>"
 }
 
 RARITY_COLOR_DICT = {
@@ -52,7 +59,13 @@ class SquanchBattleUnite(commands.Cog):
             self.characters = json.loads(data_file.read())
         self.config = Config.get_conf(self, identifier=1234567890)
         default_user = {
-            "characters" : []
+            "characters" : [],
+            "stones" : {
+                "m" : 0,
+                "mh" : 0,
+                "mhi" : 0,
+                "mhio" : 0
+            }
         }
         self.config.register_user(**default_user)
 
@@ -73,42 +86,71 @@ class SquanchBattleUnite(commands.Cog):
             summon_pool = self.get_character_pool(4)
 
         summon_character = random.choice(summon_pool)
+        message= ""
+        if await self.check_for_dupes(ctx, summon_character[0]) == True:
+            stones = await self.config.user(ctx.author).stones()
+            stones[RARITY_STONE_DICT[summon_character[1]["rarity"]]] += 1
+            await self.config.user(ctx.author).stones.set(stones)
+            message="{} You have already pulled this character. You have received 1{}!".format(ctx.author.mention, RARITY_STONE_EMOJI_DICT[summon_character[1]["rarity"]])
+        else:
+            async with self.config.user(ctx.author).characters() as characters:
+                characters.append(summon_character[0])
+            
         file = discord.File(os.path.join(os.path.dirname(__file__), summon_character[1]["imagepath"]), summon_character[1]["imagename"])
         embed = discord.Embed(title="{}".format(summon_character[1]["name"]), description="**Rarity: {} | Element: {}**".format(RARITY_DICT[summon_character[1]["rarity"]], ELEMENT_DICT[summon_character[1]["element"]]),color=RARITY_COLOR_DICT[summon_character[1]["rarity"]])
         embed.set_image(url="attachment://{}".format(summon_character[1]["imagename"]))
-
-        async with self.config.user(ctx.author).characters() as characters:
-            characters.append(summon_character[0])
-        await ctx.send(file=file, embed=embed)
+        embed.set_footer(text="Pulled by {}".format(ctx.author.name), icon_url=ctx.author.avatar_url)
+        
+        await ctx.send(file=file, embed=embed,content=message)
 
     @commands.command()
     async def chars(self, ctx):
         """Chars"""
-        async with self.config.user(ctx.author).characters() as characters:
-            current_characters = characters
-        
-        CHAR_DICT = {
-            "◀️" : menus.prev_page,
-            "⏹️" : menus.close_menu,
-            "▶️" : menus.next_page
-        }
+        try: 
+            async with self.config.user(ctx.author).characters() as characters:
+                current_characters = characters
+            
+            CHAR_DICT = {
+                "◀️" : menus.prev_page,
+                "⏹️" : menus.close_menu,
+                "▶️" : menus.next_page
+            }
 
-        pages = self.create_character_pages(ctx, current_characters)
-        if len(pages) > 1:
-            await menus.menu(ctx, pages=pages, controls=CHAR_DICT)
-        else:
-            await ctx.send(embed=pages[0])
+            pages = self.create_character_pages(ctx, current_characters)
+            if len(pages) > 1:
+                await menus.menu(ctx, pages=pages, controls=CHAR_DICT)
+            else:
+                await ctx.send(embed=pages[0])
+        except IndexError:
+            await self.error(ctx, "You have no characters to display. Please use the .pull command!")
     
     @commands.command()
     async def clear(self, ctx):
         async with self.config.user(ctx.author).characters() as characters:
-            characters = [s]
-    
+            characters.clear()
+
+    @commands.command()
+    async def stones(self, ctx):
+        stones = await self.config.user(ctx.author).stones()
+        title = "{}'s Finnathese Stones".format(ctx.author.name)
+        message = "{} {} | {} {} | {} {} | {} {}".format(RARITY_STONE_EMOJI_DICT[1], stones["m"], \
+                                                         RARITY_STONE_EMOJI_DICT[2], stones["mh"], \
+                                                         RARITY_STONE_EMOJI_DICT[3], stones["mhi"], \
+                                                         RARITY_STONE_EMOJI_DICT[4], stones["mhio"]   
+                                                         )
+        await self.message(ctx, title, message)
+
+    async def message(self, ctx, title, message, footer=""):
+        embed = discord.Embed(title=title, description=message, color=self.get_bot_color(ctx))
+        embed.set_footer(text=footer)
+
+        await ctx.send(embed=embed)
+
     async def error(self, ctx, message):
         file = discord.File(os.path.join(os.path.dirname(__file__), "images/PakEDerm.png"), "PakEDerm.png")
         embed = discord.Embed(title="Error", description=message, color=discord.Color.red())
         embed.set_thumbnail(url="attachment://PakEDerm.png")
-        discord.set_footer()
+        embed.set_footer()
 
         await ctx.send(embed=embed, file=file)
 
@@ -137,7 +179,7 @@ class SquanchBattleUnite(commands.Cog):
         return self.characters[num]
 
     def create_character_pages(self, ctx, characters):
-        embed = discord.Embed(title="{}'s Characters".format(ctx.author.name))
+        embed = discord.Embed(title="{}'s Characters".format(ctx.author.name),color=ctx.author.color)
         char_num = 1
         page_num = 0
         pages = []
@@ -165,6 +207,16 @@ class SquanchBattleUnite(commands.Cog):
             page_num += 1
         return pages
 
+    async def check_for_dupes(self, ctx, character):
+        async with self.config.user(ctx.author).characters() as characters:
+            if character in characters:
+                return True
+            else:
+                return False
+                
+    def get_bot_color(self,ctx):
+        member = ctx.message.guild.get_member(self.bot.user.id)
+        return member.color
             
     
 
