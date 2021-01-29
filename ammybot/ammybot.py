3b58e930e0968c0d9c09e873ae357b936888c58a
Unknown <jcleto@knights.ucf.edu>
2021-01-28 19:18:50 -0500

    Add !gs back in.

--- ammybot/ammybot.py
+++ ammybot/ammybot.py
@@ -213,3 +213,8 @@ class AmmyBot(commands.Cog):
     async def clayton(self, ctx):
         """"""
         await ctx.send(random.choice(clayton))
+
+    @commands.command()
+    async def gs(self, ctx):
+        """That is some good shit."""
+        await ctx.send(':ok_hand: :eyes: :ok_hand: :eyes: :ok_hand: :eyes: :ok_hand: :eyes: :ok_hand: :eyes: good shit go౦ԁ sHit :ok_hand: thats :heavy_check_mark: some good :ok_hand: :ok_hand: shit right :ok_hand: :ok_hand: there :ok_hand: :ok_hand: :ok_hand: right :heavy_check_mark: there :heavy_check_mark: :heavy_check_mark: if i do ƽaү so my self :100: i say so :100: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ :100: :ok_hand: :ok_hand: :ok_hand: НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ :ok_hand: :ok_hand: :ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand: :ok_hand: Good shit')
