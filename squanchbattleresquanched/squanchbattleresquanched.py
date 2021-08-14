d2e85beb59d5b42b1bfe59f6603cbe4183ae0b49
Yaysian <aznmyztik@gmail.com>
2021-08-14 14:51:06 -0400

    testing reaction

--- squanchbattleresquanched/squanchbattleresquanched.py
+++ squanchbattleresquanched/squanchbattleresquanched.py
@@ -17,33 +17,42 @@ class SquanchBattleResquanched(commands.Cog):
         self.config = Config.get_conf(self, identifier=1234567890)
         default_user = {
             'win': 0,
-            'loss': 0
-        }
-        default_guild = {
-            'games': []
+            'loss': 0,
+            'winquote': "Do you hear it? ＴＨＥ　ＣＲＩＥＳ　ＯＦ　ＴＨＥ　ＥＡＲＴＨ"
         }
         self.config.register_user(**default_user)
-        self.config.register_guild(**default_guild)
+        self.games = []
 
     @commands.command()
     async def battle(self, ctx):
         if ctx.message.mentions:
             if ctx.message.mentions[0].id != ctx.message.author.id:
-                logic = {
-                    'players': [self.create_player(ctx.message.author), self.create_player(ctx.message.mentions[0])],
-                    'rolls': [0, 0],
-                    'hp': [100, 100]
-                }
-
-                async with self.config.guild(ctx.guild).games() as games:
-                    games.append(logic)
-
                 title = "Battle Request"
                 message = f'{ctx.message.author.nick} has challenged {ctx.message.mentions[0].nick} to a **SQUANCH BATTLE!**\n\n⭕ to accept\n❌ to reject'
                 footer = "Squanch Battle Resquanched"
 
                 message = await self.message(ctx, title, message, footer)
+
+                logic = {
+                    'players': [ctx.message.author, ctx.message.mentions[0]],
+                    'rolls': [0, 0],
+                    'hp': [100, 100],
+                    'message': message,
+                    'started': False
+                }
+
+                self.games.append(logic)
+
                 await self.set_reactions(message, REQUEST_EMOJIS)
+
+    async def on_raw_reaction_add(self, payload):
+        if self.games.size > 0:
+            gameNum = self.isIngame(payload.user_id)
+            if gameNum:
+                if self.games[gameNum].message.id == payload.message_id:
+                    if not self.games[gameNum].started:
+                        # ACCEPT OR REJECT
+                        print(payload.emoji)
 
     async def message(self, ctx, title, message, footer=""):
         embed = discord.Embed(
@@ -56,8 +65,12 @@ class SquanchBattleResquanched(commands.Cog):
         for emoji in emojis:
             await message.add_reaction(emoji)
 
-    def create_player(self, player):
-        return {
-            'name': player.nick,
-            'id': player.id,
-        }
+    def isIngame(self, id):
+        result = False
+        count = 0
+        for game in self.games:
+            for player in game.players:
+                if player.id == id:
+                    result = count
+                count += 1
+        return result
